be032721649855cd36514a5b328ee85a6ae6b5dc
josericardo0 <gmnaarea@gmail.com>
2024-08-23 12:11:54 -0300

    continuacao dos estudos

--- src/main/java/security/SpringSecurityConfiguration.java
+++ src/main/java/security/SpringSecurityConfiguration.java
@@ -2,27 +2,56 @@ package security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
-        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-        UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder).username("Jose Ricardo").password("1234567").roles("USER", "ADMIN").build();
+        UserDetails userDetails1 = criarNovoUsuario("dindinho", "123456");
+        UserDetails userDetails2 = criarNovoUsuario("zequinha", "senha");
 
-        return new InMemoryUserDetailsManager(userDetails);
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    public UserDetails criarNovoUsuario(String usuario, String senha) {
+        Function<String, String> passwordEncoder
+                = input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username(usuario)
+                .password(senha)
+                .roles("USER","ADMIN")
+                .build();
+        return userDetails;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder () {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        return http.build();
     }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/listarTasks.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listarTasks.jsp
@@ -1,32 +1,36 @@
 <%@ include file="common/navigation.jspf" %>
 <%@ include file="common/header.jspf" %>
 
-    <div class="container">
+<div class="container">
     <h1>Suas tarefas</h1>
     <table class="table">
-    <thead>
-        <tr>
-        <th>Descricao</th>
-        <th>Data Alvo</th>
-        <th>Feito?</th>
-        </tr>
-    </thead>
-    <tbody>
-    <c:forEach items = "${tasks}" var="task">
+        <thead>
             <tr>
-            <td>${task.id}</td>
-            <td>${task.descricao}</td>
-            <td>${task.dataAlvo}</td>
-            <td>${task.feito}</td>
-            <td> <a href="deletar-task?id=${task.id}" class="btn btn-warning">Deletar</a></td>
-            <td> <a href="atualizar-task?id=${task.id}" class="btn btn-success">Atualizar</a></td>
-
+                <th>ID</th>
+                <th>Descricao</th>
+                <th>Data Alvo</th>
+                <th>Feito?</th>
+                <th>Acoes</th>
             </tr>
-    </c:forEach>
-    </tbody>
+        </thead>
+        <tbody>
+            <c:forEach items="${tasks}" var="task">
+                <tr>
+                    <td>${task.id}</td>
+                    <td>${task.descricao}</td>
+                    <td>${task.dataAlvo}</td>
+                    <td>${task.feito}</td>
+                    <td>
+                        <a href="deletar-task?id=${task.id}" class="btn btn-warning">Deletar</a>
+                        <a href="atualizar-task?id=${task.id}" class="btn btn-success">Atualizar</a>
+                    </td>
+                </tr>
+            </c:forEach>
+        </tbody>
     </table>
     <a href="add-tarefa" class="btn btn-success">Adicionar tarefa</a>
-    </div>
-    <%@ include file="common/footer.jspf" %>
+</div>
+
+<%@ include file="common/footer.jspf" %>
 </body>
 </html>
